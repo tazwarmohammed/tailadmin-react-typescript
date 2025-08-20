7806498f0a5dc5c80b16d0ab569e066d8d2c6916
Tazwar Mohammed Shoumik <tazwarmohammed1998@gmail.com>
2025-08-21 01:13:54 +0600

    Enhance SignInForm button with icon and update UserDropdown component with icons and shorten width

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -159,8 +159,10 @@ export default function SignInForm() {
         <div className="-mt-1">
           <button
             type="submit"
-            className="w-full py-2 bg-blue-400 hover:bg-white text-white hover:text-blue-400 font-semibold rounded-xl shadow-lg hover:shadow-xl transition-all duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/100 focus-visible:border-transparent"
+            className="w-full py-2 bg-blue-400 hover:bg-white text-white hover:text-blue-400 font-semibold rounded-xl shadow-lg hover:shadow-xl transition-all duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/100 focus-visible:border-transparent flex items-center justify-center gap-2"
           >
+            {/* Login Icon */}
+            <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-login-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 8v-2a2 2 0 0 1 2 -2h7a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-2" /><path d="M3 12h13l-3 -3" /><path d="M13 15l3 -3" /></svg>
             LOGIN
           </button>
         </div>
--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
+import { PencilIcon } from "../../icons";
 
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +19,7 @@ export default function UserDropdown() {
         onClick={toggleDropdown}
         className="flex items-center text-gray-700 dropdown-toggle text-theme-xs"
       >
-        <span className="block mr-2 font-medium">Musharof</span>
+        <span className="block mr-2 font-medium">tazwar123</span>
         <div className="flex items-center gap-1">
           <svg
             className="w-5 h-5 text-gray-600"
@@ -56,7 +57,7 @@ export default function UserDropdown() {
       <Dropdown
         isOpen={isOpen}
         onClose={closeDropdown}
-        className="absolute right-0 mt-[10px] flex w-[220px] flex-col rounded-xl border border-gray-200 bg-white p-1 shadow-theme-lg"
+        className="absolute right-0 mt-[10px] flex w-[170px] flex-col rounded-xl border border-gray-200 bg-white p-1 shadow-theme-lg"
       >
         <ul className="flex flex-col">
           <li>
@@ -66,6 +67,7 @@ export default function UserDropdown() {
               to="/profile"
               className="flex items-center gap-2 px-2 py-1 font-normal text-gray-700 rounded-lg group text-theme-xs hover:bg-gray-100 hover:text-gray-700"
             >
+              <PencilIcon className="w-4 h-4" />
               Edit profile
             </DropdownItem>
           </li>
@@ -76,7 +78,8 @@ export default function UserDropdown() {
               to="/profile"
               className="flex items-center gap-2 px-2 py-1 font-normal text-gray-700 rounded-lg group text-theme-xs hover:bg-gray-100 hover:text-gray-700"
             >
-              Account settings
+              <svg  xmlns="http://www.w3.org/2000/svg"  width={14}  height={14}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-settings"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
+              Account Settings
             </DropdownItem>
           </li>
           <li>
@@ -86,7 +89,8 @@ export default function UserDropdown() {
               to="/profile"
               className="flex items-center gap-2 px-2 py-1 font-normal text-gray-700 rounded-lg group text-theme-xs hover:bg-gray-100 hover:text-gray-700"
             >
-              Support
+              <svg  xmlns="http://www.w3.org/2000/svg"  width={14}  height={14}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-lock-password"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" /><path d="M8 11v-4a4 4 0 1 1 8 0v4" /><path d="M15 16h.01" /><path d="M12.01 16h.01" /><path d="M9.02 16h.01" /></svg>
+              Change password
             </DropdownItem>
           </li>
           <li>
@@ -96,7 +100,8 @@ export default function UserDropdown() {
               to="/signin"
               className="flex items-center gap-2 px-2 py-1 font-normal text-gray-700 rounded-lg group text-theme-xs hover:bg-gray-100 hover:text-gray-700"
             >
-              Sign out
+              <svg  xmlns="http://www.w3.org/2000/svg"  width={14}  height={14}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-logout"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" /><path d="M9 12h12l-3 -3" /><path d="M18 15l3 -3" /></svg>
+              Log out
             </DropdownItem>
           </li>
         </ul>
