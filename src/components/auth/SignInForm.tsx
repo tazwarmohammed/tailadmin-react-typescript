5719835288dae99e731098ff8f0cd194363a4a41
Tazwar Mohammed Shoumik <tazwarmohammed1998@gmail.com>
2025-08-22 02:41:47 +0600

    Update SignInForm layout and add iFlash title

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -91,12 +91,22 @@ export default function SignInForm() {
       `}</style>
       <div className="text-center">
         {/* Logo Section */}
-        <div className="mb-12">
+        <div className="mb-5">
           <img
             src="/images/logo/brac-bank-text-logo.svg"
             alt="BRAC Bank Logo"
-            className="mx-auto h-16 w-auto"
+            // className="mx-auto h-16 w-auto"
           />
+        </div>
+
+        {/* iFlash Title */}
+        <div className="mb-10">
+          <h2 className="text-xl font-semibold text-gray-800 whitespace-nowrap">
+            {/* steelblue/goldenrod color good for this text */}
+            <span style={{ fontFamily: "Trebuchet MS", fontSize: "1.5rem", color: "goldenrod" }}>
+              RTGS iFlash Web Portal
+            </span>
+          </h2>
         </div>
 
         {/* Sign In Form */}
@@ -108,11 +118,11 @@ export default function SignInForm() {
                 className="h-5 w-5"
                 fill="#c3cfe2ff"
                 viewBox="0 0 20 20"
-                style={{ 
-                  fill: '#374151 !important',
-                  color: '#374151 !important',
+                style={{
+                  fill: "#374151 !important",
+                  color: "#374151 !important",
                   opacity: 1,
-                  zIndex: 10
+                  zIndex: 10,
                 }}
               >
                 <path
@@ -134,22 +144,24 @@ export default function SignInForm() {
           {/* Password Field */}
           <div className="relative">
             <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none password-icon-container">
-              <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                width={20} 
-                height={20} 
-                viewBox="10 0 100 100" 
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={20}
+                height={20}
+                viewBox="10 0 100 100"
                 fill="#374151"
-                style={{ 
-                  opacity: 1,
-                  zIndex: 999,
-                  display: 'block',
-                  visibility: 'visible',
-                  minWidth: '20px',
-                  minHeight: '20px'
-                } as React.CSSProperties}
+                style={
+                  {
+                    opacity: 1,
+                    zIndex: 999,
+                    display: "block",
+                    visibility: "visible",
+                    minWidth: "20px",
+                    minHeight: "20px",
+                  } as React.CSSProperties
+                }
               >
-                <path 
+                <path
                   fill="#c3cfe2ff"
                   d="M82.105 44.218h-8.858v-8.431c.003-.036.003-.071.003-.102c0-13.073-10.636-23.71-23.713-23.71c-13.073 0-23.71 10.637-23.71 23.71v8.533h-7.931a2.62 2.62 0 0 0-2.621 2.621v38.565a2.62 2.62 0 0 0 2.621 2.621h64.21a2.62 2.62 0 0 0 2.621-2.621V46.839a2.621 2.621 0 0 0-2.622-2.621zm-42.314-8.533c0-5.375 4.371-9.741 9.746-9.741c5.341 0 9.695 4.32 9.747 9.649l-.003.031h.003v8.594H39.791v-8.533z"
                 />
